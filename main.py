0640c88ec1cae5526e3aec2297535b798c49ef9f
Gaurav Pawaskar <gauravpawaskar@Mac.attlocal.net>
2025-04-02 20:52:27 -0700

    format changes

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import logging
 app = Flask(__name__)
 hostname = ""
 
+
 @app.route('/', methods=['GET'])
 def index():
-    global hostname
+    global hostnameÏ
     logging.info(request.host)
     hostname = request.host
     return render_template('top.html')
+
 
 @app.route('/frame/<counter>', methods=['GET'])
 def frame(counter):
     logging.info(request.host)
     return render_template('frame.html', counter=counter, hostname=hostname)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
